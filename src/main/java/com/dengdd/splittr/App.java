533367840d2a51ec566ddf0bcd2c2b35adab219a
larntin <larntin2002770@gmail.com>
2017-05-09 09:03:49 +0800

    spring 工程初始化

---
+++ src/main/java/com/dengdd/splittr/App.java
@@ -0,0 +1,13 @@
+package com.dengdd.splittr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
